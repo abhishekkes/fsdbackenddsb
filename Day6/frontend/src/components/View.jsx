2693259901a569da037899cf3993784bacbb3b77
Abhishek Kesharwani <abhishekkesharwani689@gmail.com>
2025-04-01 10:00:58 +0530

    updated

--- Day6/frontend/src/components/View.jsx
+++ Day6/frontend/src/components/View.jsx
@@ -7,7 +7,7 @@ const View = () => {
         handleview();
     },[])
     const handleview= async () => {
-        const res=await axios.get('http://localhost:9000/users');
+        const res=await axios.get('https://fsdbackenddsb.onrender.com/users');
         setUsers(res.data);
         console.log(users);
     }
